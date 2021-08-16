448b2cb9e376268ca3d2613cabb6d709da716187
hexinping <hexinping456@126.com>
2021-08-16 11:54:54 +0800

    [C#] 序列化和反序列化 文件和字符串

--- 编写高质量代码：改善C#程序的157个建议（源码工程案例）/Luminji.Improve/Tip54/Program.cs
+++ 编写高质量代码：改善C#程序的157个建议（源码工程案例）/Luminji.Improve/Tip54/Program.cs
@@ -11,13 +11,24 @@ namespace Tip54
     {
         static void Main()
         {
+
+            //序列化到文件
             Person mike = new Person() { Age = 21, Name = "Mike" };
             mike.NameChanged += new EventHandler(mike_NameChanged);
             BinarySerializer.SerializeToFile(mike, @"c:\", "person.txt");
             Person p = BinarySerializer.DeserializeFromFile<Person>(@"c:\person.txt");
+            Console.WriteLine($"序列化到文件 反序列化 1======{p.Age}  {p.Name}");
             p.Name = "Rose";
-            Console.WriteLine(p.Name);
-            Console.WriteLine(p.Age.ToString());
+            Console.WriteLine($"序列化到文件 反序列化 2======{p.Age}  {p.Name}");
+
+            //序列化到字符串
+            Person mike1 = new Person() { Age = 21, Name = "Mike" };
+            string str = BinarySerializer.Serialize<Person>(mike1);
+            Console.WriteLine($"序列化到字符串  1 ====== {str}");
+
+            Person mike2 = BinarySerializer.Deserialize<Person>(str); //反序列化接口报错
+            //Console.WriteLine($"反序列化字符串  1 ====== {mike2.Age}  {mike2.Name}");
+            Console.ReadKey();
         }
 
         static void mike_NameChanged(object sender, EventArgs e)
@@ -46,6 +57,7 @@ namespace Tip54
                 name = value;
             }
         }
+
         public event EventHandler NameChanged;
     }
 
